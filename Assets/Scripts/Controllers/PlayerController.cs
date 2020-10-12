3a53debb5d6e1146c950b04948a9ad16ee0cb6dd
Javilingys <shkola.govno@gmail.com>
2020-10-12 22:24:27 +0700

    3. Added Rotation Method

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,18 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region Unity Functions
+    #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    #region Public Functions
+    #endregion
+
+    #region Private Functions
+    #endregion
 }
---
+++ Assets/Scripts/Controllers/RotatorByDrag.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class RotatorByDrag : MonoBehaviour
+{
+    [SerializeField]
+    private Transform objectForRotation = null;
+
+    #region Unity Functions
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region Public Functions
+    private void Rotate(Vector3 targetPosition, bool isOpositeDirection = false)
+    {
+        if (!objectForRotation) return;
+
+        Vector3 dir = isOpositeDirection ? objectForRotation.position - targetPosition : targetPosition - objectForRotation.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        objectForRotation.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+    #endregion
+
+    #region Private Functions
+
+    #endregion
+}
