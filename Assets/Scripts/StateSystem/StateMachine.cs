458cf6d3891d6af7a4e012d5250756a234c4ad4a
Javilingys <shkola.govno@gmail.com>
2020-10-14 14:56:25 +0700

    5. Basic State Patter Defintion

---
+++ Assets/Scripts/StateSystem/StateMachine.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine
+{
+    private IState currentState;
+
+    private Dictionary<Type, List<Transition>> transitions = new Dictionary<Type, List<Transition>>();
+    private List<Transition> currentTransitions = new List<Transition>();
+    private List<Transition> anyTransitions = new List<Transition>();
+
+    private static List<Transition> EmptyTransitions = new List<Transition>(0);
+
+    public void Tick()
+    {
+        var transition = GetTransition();
+        if (transition != null)
+            SetState(transition.To);
+
+        currentState?.Tick();
+    }
+
+    public void SetState(IState state)
+    {
+        if (state == currentState)
+            return;
+
+        currentState?.OnExit();
+        currentState = state;
+
+        transitions.TryGetValue(currentState.GetType(), out currentTransitions);
+        if (currentTransitions == null)
+            currentTransitions = EmptyTransitions;
+
+        currentState.OnEnter();
+    }
+
+    private class Transition
+    {
+
+    }
+
+    private Transition GetTransition()
+    {
+        return null;
+    }
+}
